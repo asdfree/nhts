7590c9e31b928dc428a2e67b7a9350aee1c1c9a6
Anthony Damico <ajdamico@gmail.com>
2024-11-04 18:05:40 -0500

    nhts 2022

--- tests/setup.R
+++ tests/setup.R
@@ -3,24 +3,11 @@
 # who went when where why
 library(haven)
 
-nhts_dl_uz <-
-	function( this_url ){
-	
-		tf <- tempfile()
+tf <- tempfile()
 
-		download.file( this_url , tf , mode = 'wb' )
+download.file( "https://nhts.ornl.gov/assets/2022/download/sas.zip" , tf , mode = 'wb' )
 
-		unzip( tf , exdir = tempdir() )
-	}
-	
-unzipped_survey_data <-
-	nhts_dl_uz( "https://nhts.ornl.gov/assets/2016/download/sas.zip" )
-
-unzipped_replicate_weights <-
-	nhts_dl_uz( "https://nhts.ornl.gov/assets/2016/download/Replicates.zip" )
-
-unzipped_trip_chains <-
-	nhts_dl_uz( "https://nhts.ornl.gov/assets/2016/download/TripChain/TripChain17.zip" )
+unzipped_files <- unzip( tf , exdir = tempdir() )
 nhts_import <-
 	function( this_prefix , this_unzip ){
 		
@@ -40,26 +27,24 @@ nhts_import <-
 		this_df
 	}
 	
-hhpub_df <- nhts_import( "hhpub" , unzipped_survey_data )
-perpub_df <- nhts_import( "perpub" , unzipped_survey_data )
-trippub_df <- nhts_import( "trippub" , unzipped_survey_data )
-vehpub_df <- nhts_import( "vehpub" , unzipped_survey_data )
-
-hhwgt_df <- nhts_import( "hhwgt" , unzipped_replicate_weights )
-perwgt_df <- nhts_import( "perwgt" , unzipped_replicate_weights )
+hhpub_df <- nhts_import( "hhv2pub" , unzipped_files )
+perpub_df <- nhts_import( "perv2pub" , unzipped_files )
+trippub_df <- nhts_import( "tripv2pub" , unzipped_files )
+vehpub_df <- nhts_import( "vehv2pub" , unzipped_files )
 hhpub_df[ , 'one' ] <- 1
 
 perpub_df[ , 'one' ] <- 1
 
 trippub_df[ , 'one' ] <- 1
 
-trippub_df[ !( trippub_df[ , 'trpmiles' ] %in% -9 ) , 'tripmiles_no_nines' ] <-
-	trippub_df[ !( trippub_df[ , 'trpmiles' ] %in% -9 ) , 'trpmiles' ]
+trippub_df[ !( trippub_df[ , 'trpmiles' ] %in% -9 ) , 'wtd_tripmiles_no_nines' ] <-
+	trippub_df[ !( trippub_df[ , 'trpmiles' ] %in% -9 ) , 'trpmiles' ] *
+	trippub_df[ !( trippub_df[ , 'trpmiles' ] %in% -9 ) , 'wttrdfin' ]
 trips_per_person <- 
 	with( 
 		trippub_df , 
 		aggregate( 
-			cbind( one , tripmiles_no_nines ) , 
+			cbind( wttrdfin , wtd_tripmiles_no_nines ) , 
 			list( houseid , personid ) , 
 			sum , 
 			na.rm = TRUE 
@@ -67,13 +52,13 @@ trips_per_person <-
 	)
 
 names( trips_per_person ) <-
-	c( 'houseid' , 'personid' , 'trips_per_person' , 'miles_per_person' )
+	c( 'houseid' , 'personid' , 'wtd_trips' , 'wtd_miles' )
 
 walks_per_person <- 
 	with( 
-		subset( trippub_df , trptrans == '01' ) , 
+		subset( trippub_df , trptrans == '20' ) , 
 		aggregate( 
-			cbind( one , tripmiles_no_nines ) , 
+			cbind( wttrdfin , wtd_tripmiles_no_nines ) , 
 			list( houseid , personid ) , 
 			sum , 
 			na.rm = TRUE 
@@ -81,14 +66,14 @@ walks_per_person <-
 	)
 
 names( walks_per_person ) <-
-	c( 'houseid' , 'personid' , 'walks_per_person' , 'walk_miles_per_person' )
+	c( 'houseid' , 'personid' , 'wtd_walks' , 'wtd_walk_miles' )
 nhts_df <- merge( perpub_df , trips_per_person , all.x = TRUE )
-nhts_df[ is.na( nhts_df[ , 'trips_per_person' ] ) , 'trips_per_person' ] <- 0
-nhts_df[ is.na( nhts_df[ , 'miles_per_person' ] ) , 'miles_per_person' ] <- 0
 
 nhts_df <- merge( nhts_df , walks_per_person , all.x = TRUE )
-nhts_df[ is.na( nhts_df[ , 'walks_per_person' ] ) , 'walks_per_person' ] <- 0
-nhts_df[ is.na( nhts_df[ , 'walk_miles_per_person' ] ) , 'walk_miles_per_person' ] <- 0
+
+for( this_variable in c( 'wtd_trips' , 'wtd_miles' , 'wtd_walks' , 'wtd_walk_miles' ) ){
+	nhts_df[ is.na( nhts_df[ , this_variable ] ) , this_variable ] <- 0
+}
 
 stopifnot( nrow( nhts_df ) == nrow( perpub_df ) )
 # nhts_fn <- file.path( path.expand( "~" ) , "NHTS" , "this_file.rds" )
@@ -96,47 +81,28 @@ stopifnot( nrow( nhts_df ) == nrow( perpub_df ) )
 # nhts_df <- readRDS( nhts_fn )
 library(survey)
 
-hhpub_df <- hhpub_df[ order( hhpub_df[ , 'houseid' ] ) , ]
-hhwgt_df <- hhwgt_df[ order( hhwgt_df[ , 'houseid' ] ) , ]
-
 hh_design <-
-	svrepdesign(
-		weight = ~ wthhfin ,
-		repweights =
-			hhwgt_df[ grep( 'wthhfin[0-9]' , names( hhwgt_df ) , value = TRUE ) ] ,
-		scale = 6 / 7 ,
-		rscales = 1 ,
-		type = 'JK1' ,
-		mse = TRUE ,
-		data = hhpub_df
+	svydesign( 
+		id = ~ houseid , 
+		strata = ~ stratumid , 
+		data = hhpub_df , 
+		weights = ~ wthhfin
 	)
-nhts_df <- nhts_df[ do.call( order , nhts_df[ , c( 'houseid' , 'personid' ) ] ) , ]
-perwgt_df <- perwgt_df[ do.call( order , perwgt_df[ , c( 'houseid' , 'personid' ) ] ) , ]
 
 nhts_design <-
-	svrepdesign(
-		weight = ~ wtperfin ,
-		repweights =
-			perwgt_df[ grep( 'wtperfin[0-9]' , names( perwgt_df ) , value = TRUE ) ] ,
-		scale = 6 / 7 ,
-		rscales = rep( 1 , 98 ) ,
-		type = 'JK1' ,
-		mse = TRUE ,
-		data = nhts_df
+	svydesign(
+		id = ~ houseid ,
+		strata = ~ stratumid ,
+		data = nhts_df ,
+		weights = ~ wtperfin
 	)
-trippub_df <- trippub_df[ do.call( order , trippub_df[ , c( 'houseid' , 'personid' ) ] ) , ]
-perwgt_df <- perwgt_df[ do.call( order , perwgt_df[ , c( 'houseid' , 'personid' ) ] ) , ]
 
 trip_design <-
-	svrepdesign(
-		weight = ~ wttrdfin ,
-		repweights =
-			perwgt_df[ grep( 'wttrdfin[0-9]' , names( perwgt_df ) , value = TRUE ) ] ,
-		scale = 6 / 7 ,
-		rscales = 1 ,
-		type = 'JK1' ,
-		mse = TRUE ,
-		data = trippub_df
+	svydesign(
+		id = ~ houseid ,
+		strata = ~ stratumid ,
+		data = trippub_df ,
+		weights = ~ wttrdfin
 	)
 hh_design <-
 	update(
@@ -154,8 +120,23 @@ nhts_design <-
 	update( 
 		nhts_design , 
 		
-		urban_area = as.numeric( urbrur == '01' )
+		urban_area = as.numeric( urbrur == '01' ) ,
 		
+		daily_person_trips = ( wtd_trips / ( 365 * wtperfin ) ) ,
+		
+		daily_person_miles_of_travel = ( wtd_miles / ( 365 * wtperfin ) ) ,
+		
+		daily_person_walks = ( wtd_walks / ( 365 * wtperfin ) ) ,
+		
+		daily_person_walk_miles_of_travel = ( wtd_walk_miles / ( 365 * wtperfin ) ) ,
+		
+		work_status = 
+			factor( 
+				as.numeric( worker ) , 
+				levels = 2:1 , 
+				labels = c( 'non-worker' , 'worker' ) 
+			)
+
 	)
 sum( weights( nhts_design , "sampling" ) != 0 )
 
@@ -163,22 +144,22 @@ svyby( ~ one , ~ r_sex_imp , nhts_design , unwtd.count )
 svytotal( ~ one , nhts_design )
 
 svyby( ~ one , ~ r_sex_imp , nhts_design , svytotal )
-svymean( ~ miles_per_person , nhts_design )
+svymean( ~ daily_person_walks , nhts_design )
 
-svyby( ~ miles_per_person , ~ r_sex_imp , nhts_design , svymean )
-svymean( ~ hhstate , nhts_design )
+svyby( ~ daily_person_walks , ~ r_sex_imp , nhts_design , svymean )
+svymean( ~ work_status , nhts_design , na.rm = TRUE )
 
-svyby( ~ hhstate , ~ r_sex_imp , nhts_design , svymean )
-svytotal( ~ miles_per_person , nhts_design )
+svyby( ~ work_status , ~ r_sex_imp , nhts_design , svymean , na.rm = TRUE )
+svytotal( ~ daily_person_walks , nhts_design )
 
-svyby( ~ miles_per_person , ~ r_sex_imp , nhts_design , svytotal )
-svytotal( ~ hhstate , nhts_design )
+svyby( ~ daily_person_walks , ~ r_sex_imp , nhts_design , svytotal )
+svytotal( ~ work_status , nhts_design , na.rm = TRUE )
 
-svyby( ~ hhstate , ~ r_sex_imp , nhts_design , svytotal )
-svyquantile( ~ miles_per_person , nhts_design , 0.5 )
+svyby( ~ work_status , ~ r_sex_imp , nhts_design , svytotal , na.rm = TRUE )
+svyquantile( ~ daily_person_walks , nhts_design , 0.5 )
 
 svyby( 
-	~ miles_per_person , 
+	~ daily_person_walks , 
 	~ r_sex_imp , 
 	nhts_design , 
 	svyquantile , 
@@ -186,13 +167,13 @@ svyby(
 	ci = TRUE 
 )
 svyratio( 
-	numerator = ~ walk_miles_per_person , 
-	denominator = ~ miles_per_person , 
+	numerator = ~ daily_person_walk_miles_of_travel , 
+	denominator = ~ daily_person_miles_of_travel , 
 	nhts_design 
 )
-sub_nhts_design <- subset( nhts_design , nbiketrp > 0 )
-svymean( ~ miles_per_person , sub_nhts_design )
-this_result <- svymean( ~ miles_per_person , nhts_design )
+sub_nhts_design <- subset( nhts_design , last30_bike == '01' )
+svymean( ~ daily_person_walks , sub_nhts_design )
+this_result <- svymean( ~ daily_person_walks , nhts_design )
 
 coef( this_result )
 SE( this_result )
@@ -201,7 +182,7 @@ cv( this_result )
 
 grouped_result <-
 	svyby( 
-		~ miles_per_person , 
+		~ daily_person_walks , 
 		~ r_sex_imp , 
 		nhts_design , 
 		svymean 
@@ -212,22 +193,22 @@ SE( grouped_result )
 confint( grouped_result )
 cv( grouped_result )
 degf( nhts_design )
-svyvar( ~ miles_per_person , nhts_design )
+svyvar( ~ daily_person_walks , nhts_design )
 # SRS without replacement
-svymean( ~ miles_per_person , nhts_design , deff = TRUE )
+svymean( ~ daily_person_walks , nhts_design , deff = TRUE )
 
 # SRS with replacement
-svymean( ~ miles_per_person , nhts_design , deff = "replace" )
+svymean( ~ daily_person_walks , nhts_design , deff = "replace" )
 svyciprop( ~ urban_area , nhts_design ,
 	method = "likelihood" )
-svyttest( miles_per_person ~ urban_area , nhts_design )
+svyttest( daily_person_walks ~ urban_area , nhts_design )
 svychisq( 
-	~ urban_area + hhstate , 
+	~ urban_area + work_status , 
 	nhts_design 
 )
 glm_result <- 
 	svyglm( 
-		miles_per_person ~ urban_area + hhstate , 
+		daily_person_walks ~ urban_area + work_status , 
 		nhts_design 
 	)
 
@@ -235,34 +216,48 @@ summary( glm_result )
 hhsize_counts <- svytotal( ~ hhsize_categories , hh_design )
 
 stopifnot(
-	all( round( coef( hhsize_counts ) / 1000 , 0 ) == c( 32952 , 40056 , 18521 , 26679 ) )
+	all( round( coef( hhsize_counts ) / 1000 , 0 ) == c( 36409 , 44751 , 19001 , 27384 ) )
 )
 
-hhsize_ci <- confint( hhsize_counts , df = ncol( hh_design$repweights ) )
+hhsize_ci <- confint( hhsize_counts )
 
 hhsize_moe <- hhsize_ci[ , 2 ] - coef( hhsize_counts )
 
-stopifnot( all( round( hhsize_moe / 1000 , 0 ) == c( 0 , 0 , 97 , 97 ) ) )	
-unwtd_n <- with( nhts_df , tapply( trips_per_person , worker , sum ) )
-stopifnot( all( unwtd_n == c( 79295 , 28 , 497944 , 346305 ) ) )
+stopifnot( all( round( hhsize_moe / 1000 , 0 ) == c( 1807 , 1760 , 1448 , 1742 ) ) )	
+this_mean <- svymean( ~ daily_person_trips , nhts_design )
 
-surveyed_n <- with( nhts_df , tapply( trips_per_person , worker , mean ) )
-stopifnot( all( round( surveyed_n , 2 ) == c( 2.84 , 1.65 , 3.88 , 3.21 ) ) )
+stopifnot( round( coef( this_mean ) , 2 ) == 2.28 )
 
-this_mean <- svyby( ~ trips_per_person , ~ worker , nhts_design , svymean )
-stopifnot( round( coef( this_mean ) , 2 ) == c( 2.78 , 1.28 , 3.77 , 3.01 ) )
-
-this_ci <- confint( this_mean , df = ncol( nhts_design$repweights ) )
+this_ci <- confint( this_mean )
 
 this_moe <- this_ci[ , 2 ] - coef( this_mean )
 
-stopifnot( all( round( this_moe , 2 ) == c( 0.06 , 2.21 , 0.03 , 0.06 ) ) )
+stopifnot( round( this_moe , 2 ) == 0.06 )
+
+this_mean <- svymean( ~ daily_person_miles_of_travel , nhts_design )
+
+stopifnot( round( coef( this_mean ) , 2 ) == 28.55 )
+
+this_ci <- confint( this_mean )
+
+this_moe <- this_ci[ , 2 ] - coef( this_mean )
+
+stopifnot( round( this_moe , 2 ) == 2.39 )
+this_mean <- svymean( ~ trpmiles , subset( trip_design , trpmiles >= 0 ) )
+
+stopifnot( round( coef( this_mean ) , 2 ) == 12.56 )
+
+this_ci <- confint( this_mean )
+
+this_moe <- this_ci[ , 2 ] - coef( this_mean )
+
+stopifnot( round( this_moe , 2 ) == 1.04 )
 
 library(srvyr)
 nhts_srvyr_design <- as_survey( nhts_design )
 nhts_srvyr_design %>%
-	summarize( mean = survey_mean( miles_per_person ) )
+	summarize( mean = survey_mean( daily_person_walks ) )
 
 nhts_srvyr_design %>%
 	group_by( r_sex_imp ) %>%
-	summarize( mean = survey_mean( miles_per_person ) )
+	summarize( mean = survey_mean( daily_person_walks ) )
